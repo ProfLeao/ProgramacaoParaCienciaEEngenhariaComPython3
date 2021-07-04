35725a9dde7e9825acdaa084d5202c4eab08805b
Reginaldo G Leao Jr <prof.reginaldo.leao@gmail.com>
2021-07-04 14:08:17 -0300

    Correções nos notebooks 3

---
+++ Aula03/lacos.py
@@ -0,0 +1,34 @@
+#lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#nome = "REGINALDO"
+
+#for var in lista:
+#    print(var)
+#
+#for c in nome:
+#    print(c)
+
+#for i in range(0, 11, 3):
+#    print(i)
+
+#matriculas = [123, 456, 789, 987, 654, 321]
+#nomes = ["joao", "maria", "jose", "pedro", "francisco", "ana"]
+
+#for i, (m, n)  in enumerate(zip(matriculas, nomes)):
+#    print(i+1, "  ", m, "  ", n.title())
+
+#for i, v in enumerate(matriculas):
+#    if v == 654
+#        matriculas[i] = 655
+#
+#print(matriculas)
+
+# Laço while
+#var = 100
+#while var > 0:
+#    print("Antes da subtracao: ", var)
+#    var += 10
+#    print("Apos da subtracao: ", var, "\n")
+
+# Suponha que um usuário deseje fazer a soma de n números digitados pelo 
+# usuário, e que a digitação seja interrompida apenas quando o caractere x 
+# seja informado. 
