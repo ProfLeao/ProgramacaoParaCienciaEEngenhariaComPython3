eed8196521487cb9aa400eb53df6b8257c6a6974
Reginaldo G Leao Jr <prof.reginaldo.leao@gmail.com>
2021-07-04 14:10:24 -0300

    Substituição de arquivos obsoletos

--- Aula03/ifelsebloco.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-num = int(input("Digite um número inteiro: "))
-
-if (num%2==0) and (num<20):
-    print(f"O número {num} atende a condição estabelecida.")
-else:
-    print(f"O número {num} não atende a condição estabelecida.")
